e08aff3a7e2cf51be787ecad0307d56657669a5c
yourusernameelizabethdrew <hello@elizabethdrew.co>
2022-06-06 15:32:56 +0100

    Initial Setup

---
+++ src/utils/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    token: null,
+};
+
+const reducer = (state, action) => {
+    switch(action.) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
