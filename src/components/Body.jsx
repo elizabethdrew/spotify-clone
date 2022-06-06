2d27a535382c071ae4b6d1d40e71f3cdca662219
yourusernameelizabethdrew <hello@elizabethdrew.co>
2022-06-06 18:06:22 +0100

    Page sections created and sidebar setup

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Body() {
+  return <Container>Body</Container>;
+}
+
+
+const Container = styled.div`
+
+`;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Navbar() {
+  return <Container>Navbar</Container>;
+}
+
+const Container = styled.div`
+
+`;
